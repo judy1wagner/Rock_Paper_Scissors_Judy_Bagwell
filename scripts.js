dede63871360de9c68232dca9a9346ec284c8d3d
judy1wagner <judyranae@yahoo.com>
2022-11-02 06:45:36 -0700

    Yay! I am using an object!

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 let playerSelection;
+let computerSelection;
 let game = ["rock", "paper", "scissors"];
 
 function initializeGame(){
@@ -8,31 +9,40 @@ function initializeGame(){
             document.querySelector("#button-group").appendChild(createButton("play-hand", "Play Hand"));
             listenForChoice();
             playHand();
+           
             
         });
 }
 
 function playHand(){
+    
     document.querySelector("#play-hand").addEventListener("click", () => {
         if (playerSelection === undefined) {
             document.querySelector("h2").innerHTML = "You Must Select Rock, Paper, or Scissors";
         } else {
+            //update button
+            document.querySelector("#play-hand").style.display = "none";
+            document.querySelector("#button-group").appendChild(createButton("play-again", "Play Again"));
+        
             //TODO: Remove event listeners
+
             //get computer selection
-            let computerSelection = Math.floor(Math.random() * 3);
+            computerSelection = Math.floor(Math.random() * 3);
             
             /*
-            0-2 = -2: user has rock, computer has scissors
-            0-1 = -1: user has rock, computer has paper
-            1-0 = 0: user has paper, computer has rock
-            1-2 = -1: user has paper, computer has scissors
-            2-0 = 2: user has scissors, computer has rock
+            0-2 = -2: user has rock, computer has scissors - player wins
+            2-0 = 2: user has scissors, computer has rock - computer wins
+
+            0-1 = -1: user has rock, computer has paper - computer wins
+            1-0 = 1: user has paper, computer has rock - player wins
+
+            1-2 = -1: user has paper, computer has scissors 
             2-1 = 1: user has scissors, computer has paper
             */
 
             //if it's a tie, set the color of the icon to mixed
             //and show draw message
-            if(game[computerSelection] === playerSelection.toString()){
+            if(computerSelection === game.indexOf(playerSelection.toString())){
                 document.querySelector("h2").innerHTML = "Draw!";
                 let computerIcon = document.getElementById(game[computerSelection]).children;
                 for (let item of computerIcon){
@@ -43,18 +53,44 @@ function playHand(){
                 for (let item of computerIcon){
                     item.style.color = "#D2FFAF";
                 }
+                setFinalText();
             }
-
-
-
-
-            //button for replay
-            document.querySelector("#play-hand").style.display = "none";
-            document.querySelector("#button-group").appendChild(createButton("play-again", "Play Again"));
             playAgain();
-        }
+            }
     })
 }
+
+
+//updates h2 with the appropriate message
+function setFinalText(){
+    let playerVsComputer = `${playerSelection}_${game[computerSelection]}`;
+    let results = {
+        rock_scissors: {actionTextIndex: 0, winTextIndex: 0} , 
+        scissors_paper: {actionTextIndex: 1, winTextIndex: 0}, 
+        paper_rock: {actionTextIndex: 2, winTextIndex: 0}, 
+        scissors_rock: {actionTextIndex: 0, winTextIndex: 1}, 
+        paper_scissors: {actionTextIndex: 1, winTextIndex: 1}, 
+        rock_paper: {actionTextIndex: 2, winTextIndex: 1}
+    }
+    let actionTextOptions = ["Rock crushes Scissors", "Scissors cut Paper", "Paper covers Rock"];
+    let winTextOptions = ["You Win!", "You Lose!"]
+
+    console.log(actionTextOptions[results[playerVsComputer].actionTextIndex]);
+    console.log(winTextOptions[results[playerVsComputer].winTextIndex]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //this function accepts a string representing the id of a new button and a string indicating the text to display on the button
 //it returns a new button element
